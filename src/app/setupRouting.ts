3e392c3ca9b85faad93e92008c6e8f04b0db1320
petran <petranb2@gmail.com>
2021-08-01 18:34:51 +0300

    - Add eslint script - Fix eslint warnings - Delete redundant index router - Update setupRouting.ts

--- src/app/setupRouting.ts
+++ src/app/setupRouting.ts
@@ -1,11 +1,9 @@
-import indexRouter from '@routes/index'
 import usersRouter from '@routes/users'
 
 function setupRouting(app: any) {
 
     console.log('setup routing start')
 
-    app.use('/', indexRouter);
     app.use('/users', usersRouter);
 
 
